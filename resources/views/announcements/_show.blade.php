51c2bcd74428e9d1090962f37085c97e8997b276
Alex Hampu <alex.hampu@deploy.com.ro>
2022-05-25 18:10:27 +0300

    move announcement content in a separate card

--- resources/views/announcements/_show.blade.php
+++ resources/views/announcements/_show.blade.php
@@ -3,8 +3,6 @@
         <x-hito::Form.Input title="Name" name="name" value="{{ $announcement->name }}" disabled/>
         <x-hito::Form.Input title="Description" name="description" type="textarea" :value="$announcement->description"
                             disabled/>
-        <x-hito::Form.Input title="Content" name="content" type="textarea" rich value="{{ $announcement->content }}"
-                            disabled/>
         <x-hito::Form.Input title="Publish at" name="published_at"
                             value="{{ $announcement->published_at?->format('Y-m-d H:i') }}" disabled/>
         <x-hito::Form.Input title="Pin start at" name="pin_start_at"
@@ -13,5 +11,15 @@
                             value="{{ $announcement->pin_end_at?->format('Y-m-d H:i') }}" disabled/>
         <x-hito::Form.Select.Location name="locations" multiple
                                       :value="$announcement->locations?->pluck('id')->toArray()" disabled/>
+    </div>
+</x-hito::Card>
+
+<x-hito::Card>
+    <div class="hito-admin__resource__wrapper">
+        <div class="text-xl">{{ __('app.content') }}</div>
+        <hr />
+        <div class="prose">
+            {!! $announcement->content !!}
+        </div>
     </div>
 </x-hito::Card>
